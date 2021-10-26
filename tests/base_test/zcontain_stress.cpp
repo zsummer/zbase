3436d1eac47a62588d8f2f99b9ab7438135f9e92
zsummer <yawei.zhang@foxmail.com>
2021-10-26 21:12:56 +0800

    new zhash

--- tests/base_test/zcontain_stress.cpp
+++ tests/base_test/zcontain_stress.cpp
@@ -262,7 +262,8 @@ s32 MapStress(Map& m, const std::string& desc, bool is_static = false, V* p = NU
         prof_cost.start();
         for (int i = 0; i < 1000 * 10000; i++)
         {
-            if (m.find(rand() % LOAD_CAPACITY) == m.end())
+            int r = rand() % LOAD_CAPACITY;
+            if (m.find(r) == m.end())
             {
                 LogError() << " stress test error";
             }
