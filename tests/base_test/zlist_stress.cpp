cd314282f78b46eb7ecd82484ec2ed227e579b11
zsummer <yawei.zhang@foxmail.com>
2021-10-28 14:03:20 +0800

    likely test

--- tests/base_test/zlist_stress.cpp
+++ tests/base_test/zlist_stress.cpp
@@ -19,85 +19,201 @@ using namespace zsummer;
 struct Element
 {
     int v;
-    char buf[12800];
+    char buf[8];
+    //char buf[1280];
 };
+static const int list_size = 200;
+static const int rand_size = 1000;
+static int rand_array[rand_size] = { 0 };
+
+inline bool comp(const Element& f, const Element& s)
+{
+    return f.v > s.v;
+}
 
 template<class V>
-s32 LowerBoundStress(V& v, const char* desc)
+s32 LowerBoundStress(V& v, bool is_test, const char* desc)
 {
-    auto comp = [](const Element& f, const Element& s) {return f.v > s.v; };
-    
+    Element e;
     double now = Now();
     for (size_t i = 0; i < 10000; i++)
     {
         v.clear();
-        for (u32 i = 0; i < 100; i++)
+        for (u32 i = 0; i < list_size; i++)
         {
-            Element e;
-            e.v = rand() % 50;
+            e.v = rand_array[i];
             v.insert(std::lower_bound(v.begin(), v.end(), e, comp), e);
         }
-        for (auto iter = v.begin(); iter != v.end(); ++iter)
+        if (is_test)
         {
-            auto iter2 = iter;
-            iter2++;
-            if (iter2 != v.end())
+            for (auto iter = v.begin(); iter != v.end(); ++iter)
             {
-                if (iter->v < iter2->v)
+                auto iter2 = iter;
+                iter2++;
+                if (iter2 != v.end())
                 {
-                    *(u64*)NULL = 0;
+                    if (iter->v < iter2->v)
+                    {
+                        *(u64*)NULL = 0;
+                    }
+                }
+            }
+            for (u32 i = 0; i < list_size; i++)
+            {
+                v.pop_back();
+            }
+        }
+
+    }
+    LogInfo() << desc << " lower_bound test used:" << (Now() - now)/10000 *1000*1000  << "us.";
+    now = Now();
+    for (size_t i = 0; i < 10000; i++)
+    {
+        v.clear();
+        for (u32 i = 0; i < list_size; i++)
+        {
+            e.v = rand_array[i];
+            v.push_back(e);
+        }
+        std::sort(v.begin(), v.end(), comp);
+        if (is_test)
+        {
+            for (auto iter = v.begin(); iter != v.end(); ++iter)
+            {
+                auto iter2 = iter;
+                iter2++;
+                if (iter2 != v.end())
+                {
+                    if (iter->v < iter2->v)
+                    {
+                        *(u64*)NULL = 0;
+                    }
+                }
+            }
+            for (u32 i = 0; i < list_size; i++)
+            {
+                v.pop_back();
+            }
+        }
+
+    }
+    LogInfo() << desc << " sort test used:" << (Now() - now) / 10000 * 1000 * 1000 << "us.";
+
+    return 0;
+}
+
+
+template<class V>
+s32 LowerBoundListStress(V& v, bool is_test, const char* desc)
+{
+    Element e;
+    double now = Now();
+    for (size_t i = 0; i < 10000; i++)
+    {
+        v.clear();
+        for (u32 i = 0; i < list_size; i++)
+        {
+            e.v = rand_array[i];
+            v.insert(v.lower_bound(v.begin(), v.end(), e, comp), e);
+        }
+        if (is_test)
+        {
+            for (auto iter = v.begin(); iter != v.end(); ++iter)
+            {
+                auto iter2 = iter;
+                iter2++;
+                if (iter2 != v.end())
+                {
+                    if (iter->v < iter2->v)
+                    {
+                        *(u64*)NULL = 0;
+                    }
+                }
+            }
+            for (u32 i = 0; i < list_size; i++)
+            {
+                v.pop_back();
+            }
+        }
+
+    }
+    LogInfo() << desc << " lower_bound used:" << (Now() - now) / 10000 * 1000 * 1000 << "us.";
+
+    return 0;
+}
+
+#ifndef _WINDOWS
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+#else
+#define likely(x) (x)
+#define unlikely(x) (x)
+#endif // !_WINDOWS
+
+s32 likely_test()
+{
+    volatile int likely_count = 0;
+    volatile int no_likely_count = 0;
+    if (true)
+    {
+        PROF_DEFINE_AUTO_SINGLE_RECORD(prof_cost, 1, PROF_LEVEL_NORMAL, "nolikey");
+        for (size_t i = 0; i < 10000; i++)
+        {
+            for (size_t i = 0; i < rand_size; i++)
+            {
+                if (rand_array[i]%1024 < 10)
+                {
+                    no_likely_count++;
                 }
             }
         }
-        for (u32 i = 0; i < 100; i++)
+    }
+    if (true)
+    {
+        PROF_DEFINE_AUTO_SINGLE_RECORD(prof_cost, 1, PROF_LEVEL_NORMAL, "likey");
+        for (size_t i = 0; i < 10000; i++)
         {
-            v.pop_back();
+            for (size_t i = 0; i < rand_size; i++)
+            {
+                if (likely(rand_array[i]%1024 < 10))
+                {
+                    likely_count++;
+                }
+            }
         }
     }
-    LogInfo() << desc << " lower_bound test(10000*100) used:" << Now() - now << "s.";
+    LogDebug() << "no likely hit:" << no_likely_count << "/" << 10000 * rand_size << ",  likely hit:" << likely_count << "/" << 10000 * rand_size;
     return 0;
 }
+
+
 
 s32 ZListLowerBoundTest()
 {
     std::list<Element> sys_list;
-    std::unique_ptr< zlist<Element, 100>> zlist_ptr(new  zlist<Element, 100>());
-    zlist<Element, 100>& zlist_bound = *zlist_ptr;
+    std::unique_ptr< zlist<Element, list_size>> zlist_ptr(new  zlist<Element, list_size>());
+    zlist<Element, list_size>& zlist_bound = *zlist_ptr;
     std::vector<Element> sys_vector;
-    auto comp = [](const Element& f, const Element& s) {return f.v > s.v; };
 
-    LowerBoundStress(zlist_bound, "zlist bound");
-    LowerBoundStress(sys_list, "sys_list bound");
-    LowerBoundStress(sys_vector, "sys_vector bound");
-
-    double now = Now();
-    for (size_t i = 0; i < 10000; i++)
+    std::unique_ptr< zarray<Element, list_size>> zarray_ptr(new  zarray<Element, list_size>());
+    zarray<Element, list_size>& zarray_ref = *zarray_ptr;
+    for (size_t i = 0; i < rand_size; i++)
     {
-        zlist_bound.clear();
-        for (u32 i = 0; i < 100; i++)
-        {
-            Element e;
-            e.v = rand() % 50;
-            zlist_bound.insert(zlist_bound.lower_bound(zlist_bound.begin(), zlist_bound.end(), e, comp), e);
-        }
-        for (auto iter = zlist_bound.begin(); iter != zlist_bound.end(); ++iter)
-        {
-            auto iter2 = iter;
-            iter2++;
-            if (iter2 != zlist_bound.end())
-            {
-                if (iter->v < iter2->v)
-                {
-                    *(u64*)NULL = 0;
-                }
-            }
-        }
-        for (u32 i = 0; i < 100; i++)
-        {
-            zlist_bound.pop_back();
-        }
+        rand_array[i] = rand();
     }
-    LogInfo() << "zlist_bound inner lower_bound test(10000*100) used:" << Now() - now << "s.";
+
+    LowerBoundStress(sys_vector, true, "sys_vector");
+    LowerBoundStress(zarray_ref, true, "zarray");
+
+    LowerBoundStress(sys_vector, false, "sys_vector");
+    LowerBoundStress(zarray_ref, false, "zarray");
+
+
+    LowerBoundListStress(zlist_bound, true,  "zlist");
+    LowerBoundListStress(zlist_bound, false,  "zlist");
+
+
+    likely_test();
 
     return 0;
 }
