3275d64180933bf0f957cfdcd512c875e3f9349c
zsummer <yawei.zhang@foxmail.com>
2022-05-16 19:44:31 +0800

    foreach wrap

--- tests/foreach_test/foreach_test.cpp
+++ tests/foreach_test/foreach_test.cpp
@@ -34,12 +34,12 @@ typedef float f32;
 #include "zlist.h"
 #include "zarray.h"
 using namespace zsummer;
-s32 tick1000(u64 key, u64 id, s64 now_ms)
+s32 tick1000(u64 id, s64 now_ms)
 {
     LogDebug() << "tick1000: id:" << id ;
     return 0;
 }
-s32 tick5000(u64 key, u64 id, s64 now_ms)
+s32 tick5000(u64 id, s64 now_ms)
 {
     LogDebug() << "tick5000: id:" << id;
     return 0;
@@ -74,6 +74,41 @@ struct node
     trigger_info triggers[TT_MAX];
 };
 
+class obj_foreach : public zforeach
+{
+public:
+    using ForeachTick = s32(*)(u64, s64);
+    inline s32 init(u64 key, ForeachTick trigger, u32 begin_id, u32 end_id, u32 base_tick, u32 foreach_tick)
+    {
+        s32 ret = zforeach::init(key, (u64)(void*)trigger, begin_id, end_id, &obj_foreach::hook, base_tick, foreach_tick);
+        return ret;
+    }
+
+    inline s32 window_tick(u32 begin_id, u32 end_id, s64 now_ms)
+    {
+        subframe_.soft_begin_ = begin_id;
+        subframe_.soft_end_ = end_id;
+        return zforeach_impl::window_tick(subframe_, now_ms);
+    }
+
+    static inline s32 hook(const zforeach_impl::subframe& sub, u32 begin_id, u32 end_id, s64 now_ms)
+    {
+        ForeachTick of = (ForeachTick)(void*)sub.userdata_;
+        if (of == NULL)
+        {
+            return -1;
+        }
+        for (u32 i = begin_id; i < end_id; i++)
+        {
+            (of)(i, now_ms);
+        }
+        return 0;
+    }
+};
+
+
+
+
 void trigger_check_init(node& node, s64 now_ms)
 {
     memset(&node, 0, sizeof(node));
@@ -95,9 +130,10 @@ using obj_pool = zarray<node, node_count>;
 static obj_pool g_pool;
 
 template<TRIGGER_TYPE TT, u32 INTERVAL>
-static inline s32 trigger_check(u64 key, u64 id, s64 now_ms)
+static inline s32 trigger_check(u64 pid, s64 now_ms)
 {
-    obj_pool* p = (obj_pool*)key;
+    u64 id = (u64)pid;
+    obj_pool* p = (obj_pool*)&g_pool;
     if (!p->at(id).valid)
     {
         return 0;
@@ -141,6 +177,18 @@ static inline s32 trigger_check(u64 key, u64 id, s64 now_ms)
 
 
 
+template<TRIGGER_TYPE TT, u32 INTERVAL>
+static inline s32 trigger_empty(u64 pid, s64 now_ms)
+{
+    u64 id = (u64)pid;
+    obj_pool* p = (obj_pool*)&g_pool;
+    if (!p->at(id).valid)
+    {
+        return 0;
+    }
+    return 0;
+}
+
 
 
 
@@ -164,18 +212,18 @@ int main(int argc, char *argv[])
     if (true)
     {
         g_pool.clear();
-        zforeach fe[TT_MAX];
-        s32 ret = fe[TT_10MS].init((u64)&g_pool, 0, g_pool.max_size(), trigger_check<TT_10MS, 10>, 10, 10);
-        ret |= fe[TT_50_1MS].init((u64)&g_pool, 0, g_pool.max_size(), trigger_check<TT_50_1MS, 50>, 10, 50);
-        ret |= fe[TT_50_2MS].init((u64)&g_pool, 0, g_pool.max_size(), trigger_check<TT_50_2MS, 50>, 10, 50);
-        ret |= fe[TT_800MS].init((u64)&g_pool, 0, g_pool.max_size(), trigger_check<TT_800MS, 800>, 10, 800);
+        obj_foreach fe[TT_MAX];
+        s32 ret = fe[TT_10MS].init((u64)&g_pool, trigger_check<TT_10MS, 10>, 0, g_pool.max_size(), 10, 10);
+        ret |= fe[TT_50_1MS].init((u64)&g_pool, trigger_check<TT_50_1MS, 50>, 0, g_pool.max_size(), 10, 50);
+        ret |= fe[TT_50_2MS].init((u64)&g_pool, trigger_check<TT_50_2MS, 50>, 0, g_pool.max_size(), 10, 50);
+        ret |= fe[TT_800MS].init((u64)&g_pool,  trigger_check<TT_800MS, 800>, 0, g_pool.max_size(), 10, 800);
         if (ret != 0)
         {
             LogError() << "";
             return -2;
         }
         s64 now_ms = 1000000;
-        PROF_DEFINE_AUTO_MULTI_ANON_RECORD(cost, 1000, "1000 auto inc");
+        PROF_DEFINE_AUTO_MULTI_ANON_RECORD(cost, 1000, "1000 trigger_check mix auto inc");
         for (u32 i = 0; i < 1000; i++)
         {
             now_ms += 10;
@@ -195,18 +243,18 @@ int main(int argc, char *argv[])
     if (true)
     {
         g_pool.clear();
-        zforeach fe[TT_MAX];
-        s32 ret = fe[TT_10MS].init((u64)&g_pool, 0, g_pool.max_size(), trigger_check<TT_10MS, 10>, 10, 10);
-        ret |= fe[TT_50_1MS].init((u64)&g_pool, 0, g_pool.max_size(), trigger_check<TT_50_1MS, 50>, 10, 50);
-        ret |= fe[TT_50_2MS].init((u64)&g_pool, 0, g_pool.max_size(), trigger_check<TT_50_2MS, 50>, 10, 50);
-        ret |= fe[TT_800MS].init((u64)&g_pool, 0, g_pool.max_size(), trigger_check<TT_800MS, 800>, 10, 800);
+        obj_foreach fe[TT_MAX];
+        s32 ret = fe[TT_10MS].init((u64)&g_pool, trigger_check<TT_10MS, 10>, 0, g_pool.max_size(), 10, 10);
+        ret |= fe[TT_50_1MS].init((u64)&g_pool, trigger_check<TT_50_1MS, 50>, 0, g_pool.max_size(), 10, 50);
+        ret |= fe[TT_50_2MS].init((u64)&g_pool, trigger_check<TT_50_2MS, 50>, 0, g_pool.max_size(), 10, 50);
+        ret |= fe[TT_800MS].init((u64)&g_pool, trigger_check<TT_800MS, 800>, 0, g_pool.max_size(), 10, 800);
         if (ret != 0)
         {
             LogError() << "";
             return -2;
         }
         s64 now_ms = 1000;
-        PROF_DEFINE_AUTO_MULTI_ANON_RECORD(cost, 1000, "1000 full");
+        PROF_DEFINE_AUTO_MULTI_ANON_RECORD(cost, 1000, "1000  trigger_check  mix full");
         for (u32 i = 0; i < 1000; i++)
         {
             now_ms += 10;
@@ -226,11 +274,11 @@ int main(int argc, char *argv[])
     if (true)
     {
         g_pool.clear();
-        zforeach fe[TT_MAX];
-        s32 ret = fe[TT_10MS].init((u64)&g_pool, 0, g_pool.max_size(), trigger_check<TT_10MS, 10>, 10, 10);
-        ret |= fe[TT_50_1MS].init((u64)&g_pool, 0, g_pool.max_size(), trigger_check<TT_50_1MS, 50>, 10, 50);
-        ret |= fe[TT_50_2MS].init((u64)&g_pool, 0, g_pool.max_size(), trigger_check<TT_50_2MS, 50>, 10, 50);
-        ret |= fe[TT_800MS].init((u64)&g_pool, 0, g_pool.max_size(), trigger_check<TT_800MS, 800>, 10, 800);
+        obj_foreach fe[TT_MAX];
+        s32 ret = fe[TT_10MS].init((u64)&g_pool, trigger_check<TT_10MS, 10>, 0, g_pool.max_size(), 10, 10);
+        ret |= fe[TT_50_1MS].init((u64)&g_pool, trigger_check<TT_50_1MS, 50>, 0, g_pool.max_size(), 10, 50);
+        ret |= fe[TT_50_2MS].init((u64)&g_pool, trigger_check<TT_50_2MS, 50>, 0, g_pool.max_size(), 10, 50);
+        ret |= fe[TT_800MS].init((u64)&g_pool, trigger_check<TT_800MS, 800>, 0, g_pool.max_size(), 10, 800);
         if (ret != 0)
         {
             LogError() << "";
@@ -241,7 +289,7 @@ int main(int argc, char *argv[])
         for (u32 i = 0; i < 100000; i++)
         {
             now_ms += 10;
-            for (size_t tt = 0; tt < TT_10MS + 1; tt++)
+            for (size_t tt = TT_10MS; tt < TT_10MS + 1; tt++)
             {
                 fe[tt].window_tick(0, g_pool.size(), now_ms);
             }
@@ -256,11 +304,11 @@ int main(int argc, char *argv[])
     if (true)
     {
         g_pool.clear();
-        zforeach fe[TT_MAX];
-        s32 ret = fe[TT_10MS].init((u64)&g_pool, 0, g_pool.max_size(), trigger_check<TT_10MS, 10>, 10, 10);
-        ret |= fe[TT_50_1MS].init((u64)&g_pool, 0, g_pool.max_size(), trigger_check<TT_50_1MS, 50>, 10, 50);
-        ret |= fe[TT_50_2MS].init((u64)&g_pool, 0, g_pool.max_size(), trigger_check<TT_50_2MS, 50>, 10, 50);
-        ret |= fe[TT_800MS].init((u64)&g_pool, 0, g_pool.max_size(), trigger_check<TT_800MS, 800>, 10, 800);
+        obj_foreach fe[TT_MAX];
+        s32 ret = fe[TT_10MS].init((u64)&g_pool, trigger_check<TT_10MS, 10>, 0, g_pool.max_size(), 10, 10);
+        ret |= fe[TT_50_1MS].init((u64)&g_pool, trigger_check<TT_50_1MS, 50>, 0, g_pool.max_size(), 10, 50);
+        ret |= fe[TT_50_2MS].init((u64)&g_pool, trigger_check<TT_50_2MS, 50>, 0, g_pool.max_size(), 10, 50);
+        ret |= fe[TT_800MS].init((u64)&g_pool, trigger_check<TT_800MS, 800>, 0, g_pool.max_size(), 10, 800);
         if (ret != 0)
         {
             LogError() << "";
@@ -286,11 +334,11 @@ int main(int argc, char *argv[])
     if (true)
     {
         g_pool.clear();
-        zforeach fe[TT_MAX];
-        s32 ret = fe[TT_10MS].init((u64)&g_pool, 0, g_pool.max_size(), trigger_check<TT_10MS, 10>, 10, 10);
-        ret |= fe[TT_50_1MS].init((u64)&g_pool, 0, g_pool.max_size(), trigger_check<TT_50_1MS, 50>, 10, 50);
-        ret |= fe[TT_50_2MS].init((u64)&g_pool, 0, g_pool.max_size(), trigger_check<TT_50_2MS, 50>, 10, 50);
-        ret |= fe[TT_800MS].init((u64)&g_pool, 0, g_pool.max_size(), trigger_check<TT_800MS, 800>, 10, 800);
+        obj_foreach fe[TT_MAX];
+        s32 ret = fe[TT_10MS].init((u64)&g_pool, trigger_check<TT_10MS, 10>, 0, g_pool.max_size(), 10, 10);
+        ret |= fe[TT_50_1MS].init((u64)&g_pool, trigger_check<TT_50_1MS, 50>, 0, g_pool.max_size(), 10, 50);
+        ret |= fe[TT_50_2MS].init((u64)&g_pool, trigger_check<TT_50_2MS, 50>, 0, g_pool.max_size(), 10, 50);
+        ret |= fe[TT_800MS].init((u64)&g_pool, trigger_check<TT_800MS, 800>, 0, g_pool.max_size(), 10, 800);
         if (ret != 0)
         {
             LogError() << "";
@@ -314,21 +362,53 @@ int main(int argc, char *argv[])
         }
     }
 
+
     if (true)
     {
         g_pool.clear();
-        zforeach fe[TT_MAX];
-        s32 ret = fe[TT_10MS].init((u64)&g_pool, 0, g_pool.max_size(), trigger_check<TT_10MS, 10>, 10, 10);
-        ret |= fe[TT_50_1MS].init((u64)&g_pool, 0, g_pool.max_size(), trigger_check<TT_50_1MS, 50>, 10, 50);
-        ret |= fe[TT_50_2MS].init((u64)&g_pool, 0, g_pool.max_size(), trigger_check<TT_50_2MS, 50>, 10, 50);
-        ret |= fe[TT_800MS].init((u64)&g_pool, 0, g_pool.max_size(), trigger_check<TT_800MS, 800>, 10, 800);
+        obj_foreach fe[TT_MAX];
+        s32 ret = fe[TT_10MS].init((u64)&g_pool, trigger_check<TT_10MS, 10>, 0, g_pool.max_size(), 10, 10);
+        ret |= fe[TT_50_1MS].init((u64)&g_pool, trigger_check<TT_50_1MS, 50>, 0, g_pool.max_size(), 10, 50);
+        ret |= fe[TT_50_2MS].init((u64)&g_pool, trigger_check<TT_50_2MS, 50>, 0, g_pool.max_size(), 10, 50);
+        ret |= fe[TT_800MS].init((u64)&g_pool, trigger_check<TT_800MS, 800>, 0, g_pool.max_size(), 10, 800);
         if (ret != 0)
         {
             LogError() << "";
             return -2;
         }
         s64 now_ms = 1000000;
-        PROF_DEFINE_AUTO_MULTI_ANON_RECORD(cost, 100000, "100000");
+        PROF_DEFINE_AUTO_MULTI_ANON_RECORD(cost, 100000, "100000 mix trigger_check");
+        for (u32 i = 0; i < 100000; i++)
+        {
+            now_ms += 10;
+            for (size_t tt = 0; tt < TT_MAX; tt++)
+            {
+                fe[tt].window_tick(0, g_pool.size(), now_ms);
+            }
+            if (!g_pool.full() && rand() % 2 == 0)
+            {
+                node n;
+                trigger_check_init(n, now_ms);
+                g_pool.push_back(n);
+            }
+        }
+    }
+
+    if (true)
+    {
+        g_pool.clear();
+        obj_foreach fe[TT_MAX];
+        s32 ret = fe[TT_10MS].init((u64)&g_pool, trigger_empty<TT_10MS, 10>, 0, g_pool.max_size(), 10, 10);
+        ret |= fe[TT_50_1MS].init((u64)&g_pool, trigger_empty<TT_50_1MS, 50>, 0, g_pool.max_size(), 10, 50);
+        ret |= fe[TT_50_2MS].init((u64)&g_pool, trigger_empty<TT_50_2MS, 50>, 0, g_pool.max_size(), 10, 50);
+        ret |= fe[TT_800MS].init((u64)&g_pool, trigger_empty<TT_800MS, 800>, 0, g_pool.max_size(), 10, 800);
+        if (ret != 0)
+        {
+            LogError() << "";
+            return -2;
+        }
+        s64 now_ms = 1000000;
+        PROF_DEFINE_AUTO_MULTI_ANON_RECORD(cost, 100000, "100000 mix trigger_empty");
         for (u32 i = 0; i < 100000; i++)
         {
             now_ms += 10;
@@ -347,12 +427,11 @@ int main(int argc, char *argv[])
 
 
 
-
     if (true)
     {
-        zforeach t[2];
-        s32 ret = t[0].init(0, 100, 110, &tick1000, 100, 1000);
-        ret |= t[1].init(0, 100, 110, &tick5000, 100, 5000);
+        obj_foreach t[2];
+        s32 ret = t[0].init(0, &tick1000, 100, 110, 100, 1000);
+        ret |= t[1].init(0, &tick5000, 100, 110, 100, 5000);
         if (ret != 0)
         {
             LogError() << "init error:" << ret;
@@ -365,11 +444,28 @@ int main(int argc, char *argv[])
             t[1].window_tick(100, 110, i);
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
-
     }
 
 
+    if (true)
+    {
+        obj_foreach t[2];
+        s32 ret = t[0].init(0, &tick1000, 100, 150, 100, 1000);
+        ret |= t[1].init(0, &tick5000, 100, 150, 100, 5000);
+        if (ret != 0)
+        {
+            LogError() << "init error:" << ret;
+            return -2;
+        }
 
+        for (size_t i = 0; i < 100; i++)
+        {
+            t[0].window_tick(100, 150, i);
+            t[1].window_tick(100, 150, i);
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+
+    }
 
 
 
