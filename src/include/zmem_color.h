00a8f80d36eaaf3b3432d5a4af0b55c14d2210a1
zsummer <yawei.zhang@foxmail.com>
2022-05-25 17:03:58 +0800

    add string shm

--- src/include/zmem_color.h
+++ src/include/zmem_color.h
@@ -37,10 +37,12 @@ namespace zsummer
         MEM_COLOR_SET = 4,
         MEM_COLOR_LIST = 5,
         MEM_COLOR_SEG_LIST = 6,
+        MEM_COLOR_QUEUE = 7,
         MEM_COLOR_MAX,
     };
 
     static_assert(MEM_COLOR_MAX < zmalloc::CHUNK_COLOR_MASK / 2, "color max");
+
     using shm_string = std::basic_string<char, std::char_traits<char>, zallocator<char, MEM_COLOR_STRING> >;
 
     template<class _Ty>
@@ -48,15 +50,29 @@ namespace zsummer
 
     template<class K, class T, class P = std::less<K> >
     using shm_map = std::map<K, T, P, zallocator<std::pair<const K, T>, MEM_COLOR_MAP> >;
+    template<class K, class T, class P = std::less<K> >
+    using shm_multimap = std::multimap<K, T, P, zallocator<std::pair<const K, T>, MEM_COLOR_MAP> >;
+    template<class K, class T>
+    using shm_unordered_map = std::unordered_map< K, T, std::hash<K>, std::equal_to<K>, zallocator<std::pair<const K, T>, MEM_COLOR_MAP> >;
+
 
     template<class K, class P = std::less<K>>
     using shm_set = std::set<K, P, zallocator<K, MEM_COLOR_SET> >;
+    template<class K>
+    using shm_unordered_set = std::unordered_set< K, std::hash<K>, std::equal_to<K>, zallocator<const K, MEM_COLOR_SET> >;
+
 
     template<class _Ty>
     using shm_list = std::list<_Ty, zallocator<_Ty, MEM_COLOR_LIST>>;
 
     template<class _Ty, size_t _Size, size_t _FixedSize>
     using shm_zlist_ext = zlist_ext<_Ty, _Size, _FixedSize, zallocator<typename std::aligned_storage<sizeof(_Ty), alignof(_Ty)>::type, MEM_COLOR_SEG_LIST>>;
+
+    template<class _Ty>
+    using shm_deque = std::deque<_Ty, zallocator<_Ty, MEM_COLOR_QUEUE>>;
+
+    template<class _Ty>
+    using shm_queue = std::queue<_Ty, shm_deque<_Ty>>;
 
     //unresumable area.  
 
