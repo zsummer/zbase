d67bd933a4e70ec240e9177f390620c682cb73e2
zsummer <yawei.zhang@foxmail.com>
2023-05-21 02:33:17 +0800

    zbitset benchmark

--- tests/common/test_common.h
+++ tests/common/test_common.h
@@ -1,6 +1,6 @@
 
 /*
-* zcontain License
+* test_common License
 * Copyright (C) 2019 YaweiZhang <yawei.zhang@foxmail.com>.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,134 +18,18 @@
 
 
 
-#define OI 0
 
-#if defined(__GCC__) && (OI)
-#pragma GCC push_options
-#pragma GCC optimize(2)
-#pragma GCC optimize(3)
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize("inline")
-#pragma GCC optimize("-fgcse")
-#pragma GCC optimize("-fgcse-lm")
-#pragma GCC optimize("-fipa-sra")
-#pragma GCC optimize("-ftree-pre")
-#pragma GCC optimize("-ftree-vrp")
-#pragma GCC optimize("-fpeephole2")
-#pragma GCC optimize("-ffast-math")
-#pragma GCC optimize("-fsched-spec")
-#pragma GCC optimize("unroll-loops")
-#pragma GCC optimize("-falign-jumps")
-#pragma GCC optimize("-falign-loops")
-#pragma GCC optimize("-falign-labels")
-#pragma GCC optimize("-fdevirtualize")
-#pragma GCC optimize("-fcaller-saves")
-#pragma GCC optimize("-fcrossjumping")
-#pragma GCC optimize("-fthread-jumps")
-#pragma GCC optimize("-funroll-loops")
-#pragma GCC optimize("-fwhole-program")
-#pragma GCC optimize("-freorder-blocks")
-#pragma GCC optimize("-fschedule-insns")
-#pragma GCC optimize("inline-functions")
-#pragma GCC optimize("-ftree-tail-merge")
-#pragma GCC optimize("-fschedule-insns2")
-#pragma GCC optimize("-fstrict-aliasing")
-#pragma GCC optimize("-fstrict-overflow")
-#pragma GCC optimize("-falign-functions")
-#pragma GCC optimize("-fcse-skip-blocks")
-#pragma GCC optimize("-fcse-follow-jumps")
-#pragma GCC optimize("-fsched-interblock")
-#pragma GCC optimize("-fpartial-inlining")
-#pragma GCC optimize("no-stack-protector")
-#pragma GCC optimize("-freorder-functions")
-#pragma GCC optimize("-findirect-inlining")
-#pragma GCC optimize("-fhoist-adjacent-loads")
-#pragma GCC optimize("-frerun-cse-after-loop")
-#pragma GCC optimize("inline-small-functions")
-#pragma GCC optimize("-finline-small-functions")
-#pragma GCC optimize("-ftree-switch-conversion")
-#pragma GCC optimize("-foptimize-sibling-calls")
-#pragma GCC optimize("-fexpensive-optimizations")
-#pragma GCC optimize("-funsafe-loop-optimizations")
-#pragma GCC optimize("inline-functions-called-once")
-#pragma GCC optimize("-fdelete-null-pointer-checks")
-
-#pragma GCC target("avx", "sse2")
-
-#endif
-
-
-#include "fn_log.h"
+#include "ztest.h"
 #include "zarray.h"
 #include "zlist.h"
 #include "zhash_map.h"
-#ifndef WIN32
-#include <cxxabi.h>
-#endif
-using s8 = char;
-using u8 = unsigned char;
-using s16 = short int;
-using u16 = unsigned short int;
-using s32 = int;
-using u32 = unsigned int;
-using s64 = long long;
-using u64 = unsigned long long;
-using f32 = float;
-using f64 = double;
-
-#ifndef  ZCONTAIN_H
-#define ZCONTAIN_H
 
 
 
+#ifndef  TEST_COMMON_H
+#define TEST_COMMON_H
 
 
-template<typename ... Args>
-FNLog::LogStream& ASSERT_ARGS_LOG(FNLog::LogStream&& ls, const std::string& head, Args&& ... args)
-{
-    ls << head << " ";
-    std::initializer_list<int>{ (ls << args, '\0') ... };
-    return ls;
-}
-
-#define ASSERT_TEST(expr, ...)  \
-do \
-{\
-    if(expr) \
-    { \
-        std::string expr_str = #expr; \
-        ASSERT_ARGS_LOG(std::move(LogDebug()), expr_str, ##__VA_ARGS__, " ok."); \
-    }\
-    else \
-    {\
-        std::string expr_str = #expr; \
-        ASSERT_ARGS_LOG(std::move(LogError()), expr_str, ##__VA_ARGS__); \
-        return -1; \
-    }\
-}\
-while(0)
-
-#define ASSERT_TEST_NOLOG(expr, ...)  \
-do \
-{\
-    if(!(expr)) \
-    {\
-        std::string expr_str = #expr; \
-        ASSERT_ARGS_LOG(std::move(LogError()), expr_str, ##__VA_ARGS__); \
-        return -1; \
-    }\
-}\
-while(0)
-
-
-
-#define ASSERT_TEST_EQ(val1, val2, ...)   \
-{\
-    auto v1 = (val1); \
-    auto v_tmp = (val2); \
-    auto v2 = static_cast<decltype(v1)>(v_tmp); \
-    ASSERT_TEST_NOLOG((v1)==(v2), (v1) , " ",  (v2), ##__VA_ARGS__);\
-}
 
 
 
@@ -252,15 +136,5 @@ do \
 #define CheckRAIIValByType(t)   ASSERT_RAII_VAL(TypeName<t>());
 
 
-
-static inline void FNLogFunc(const ProfSerializer& serializer)
-{
-    LOG_STREAM_DEFAULT_LOGGER(0, FNLog::PRIORITY_DEBUG, 0, 0, FNLog::LOG_PREFIX_NULL).write_buffer(serializer.buff(), (int)serializer.offset());
-}
-
-
-#if defined(__GCC__) && (OI)
-#pragma GCC pop_options
-#endif
 
 #endif
