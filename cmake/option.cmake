d3c1bc8c1529a9a4ed51bb2fa34d9bd36827887e
skylerzhang <skylerzhang@tencent.com>
2023-06-09 21:29:20 +0800

    update

--- cmake/option.cmake
+++ cmake/option.cmake
@@ -1,4 +1,47 @@
 
+
+#from github.com/zsummer
+
+# plat : WIN32 APPLE UNIX    (UNIX contain UNIX like)
+
+# CMAKE_CXX_COMPILER_ID:  GNU Intel Clang AppleClang MSVC 
+# example IF (CMAKE_CXX_COMPILER_ID MATCHES "Clang") ENDIF()
+# SET(CMAKE_C_COMPILER /usr/bin/gcc-8)
+# SET(CMAKE_CXX_COMPILER /usr/bin/g++-8)
+# jump compiler works check
+# if(WIN32)
+#    set(CMAKE_C_COMPILER_WORKS TRUE)
+#    set(CMAKE_CXX_COMPILER_WORKS TRUE)
+# endif(WIN32)
+
+# jump this project build when msvc 
+# set_target_properties(${PROJECT_NAME} PROPERTIES EXCLUDE_FROM_ALL 1 EXCLUDE_FROM_DEFAULT_BUILD 1)
+
+# set this project setup build when msvc 
+# set_property(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} PROPERTY VS_STARTUP_PROJECT ${PROJECT_NAME})
+
+# show msvc folder
+#  SET_PROPERTY(GLOBAL PROPERTY USE_FOLDERS ON) 
+
+# use C++ 14
+# set(CMAKE_CXX_FLAGS -std=c++14) 
+
+# 
+# CMAKE_SOURCE_DIR   cmake root dir 
+# CMAKE_CURRENT_SOURCE_DIR current cmakelist.txt dir  
+# EXECUTABLE_OUTPUT_PATH can set it change bin out dir
+# CMAKE_MODULE_PATH can set it change module dir 
+# PROJECT_NAME cur project name 
+
+# include 
+# include_directories  
+# link_directories 
+# link_libraries 
+
+# 
+# execute_process
+
+
 set(CMAKE_VERBOSE_MAKEFILEON ON)
 if( NOT CMAKE_CONFIGURATION_TYPES )
     set( CMAKE_CONFIGURATION_TYPES Debug Release )
@@ -23,8 +66,11 @@ endif()
 
 #输出
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${PROJECT_SOURCE_DIR}/bin)
-
-
+SET(CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}")
+SET(CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}")
+SET(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_SOURCE_DIR}/bin)
+SET(CMAKE_LIBRARY_OUTPUT_DIRECTORY_DEBUG "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}")
+SET(CMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}")
 
 
 
