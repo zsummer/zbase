8cebde38ff44686bff3d7fb2ca0c0aceb8a7148f
skylerzhang <skylerzhang@tencent.com>
2023-06-25 12:29:21 +0800

    release rule

--- release_version.sh
+++ release_version.sh
@@ -13,27 +13,41 @@ cp LICENSE ./dist/include/zbase/
 
 last_sha1=`git rev-parse HEAD`
 last_date=`git show --pretty=format:"%ci" | head -1`
+last_diff=`git log -1 --stat `
 
 last_dist_sha1=`git log -1 --stat ./src |grep -E "commit ([0-9a-f]*)" |grep -E -o "[0-9a-f]{10,}"`
 last_dist_date=`git show $last_dist_sha1 --pretty=format:"%ci" | head -1`
-echo "zbase last commit:"
+last_dist_diff=`git log -1 --stat ./src`
+
+echo ""
+echo "[zbase last commit]:"
 echo $last_sha1
 echo $last_date
-git log -1 --stat ./src
+echo ""
+echo "[zbase last diff]:"
+echo $last_diff
 
 
-echo "./src last commit:"
+echo ""
+echo "[./src last commit]:"
 echo $last_dist_sha1
 echo $last_dist_date
-git log -1 --stat ./src
+echo ""
+echo "[./src last diff]:"
+echo $last_dist_diff
 
-echo "write versions"
+echo ""
+echo "[write versions]"
 echo "version:" > ./dist/include/zbase/VERSION
 echo "last_sha1(./src)=$last_dist_sha1" >> ./dist/include/zbase/VERSION 
 echo "last_date(./src)=$last_dist_date" >> ./dist/include/zbase/VERSION 
 echo "" >> ./dist/include/zbase/VERSION 
 echo "git log -1 --stat ./src:" >> ./dist/include/zbase/VERSION 
-echo "write done"
+echo $last_dist_diff >> ./dist/include/zbase/VERSION
+cat ./dist/include/zbase/VERSION
+
+echo ""
+echo "[write done]"
 
 
 
