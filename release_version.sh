6a05d678291400ca18568a245b5e7d73078d700f
zsummer <yawei.zhang@foxmail.com>
2023-06-25 11:30:35 +0800

    new release rule

--- release_version.sh
+++ release_version.sh
@@ -13,9 +13,28 @@ cp LICENSE ./dist/include/zbase/
 
 last_sha1=`git rev-parse HEAD`
 last_date=`git show --pretty=format:"%ci" | head -1`
+
+last_dist_sha1=`git log -1 --stat ./src |grep -E "commit ([0-9a-f]*)" |grep -E -o "[0-9a-f]{10,}"`
+last_dist_date=`git show $last_dist_sha1 --pretty=format:"%ci" | head -1`
+echo "zbase last commit:"
+echo $last_sha1
+echo $last_date
+git log -1 --stat ./src
+
+
+echo "./src last commit:"
+echo $last_dist_sha1
+echo $last_dist_date
+git log -1 --stat ./src
+
+echo "write versions"
 echo "version:" > ./dist/include/zbase/VERSION
-echo "last_sha1=$last_sha1" >> ./dist/include/zbase/VERSION 
-echo "last_date=$last_date" >> ./dist/include/zbase/VERSION 
+echo "last_sha1(./src)=$last_dist_sha1" >> ./dist/include/zbase/VERSION 
+echo "last_date(./src)=$last_dist_date" >> ./dist/include/zbase/VERSION 
 echo "" >> ./dist/include/zbase/VERSION 
-echo "git log:" >> ./dist/include/zbase/VERSION 
-git log -1 --stat ./src >> ./dist/include/zbase/VERSION 
+echo "git log -1 --stat ./src:" >> ./dist/include/zbase/VERSION 
+echo "write done"
+
+
+
+
