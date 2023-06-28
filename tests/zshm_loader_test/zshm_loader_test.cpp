6c4386da83346e20a5e8fd966c0da258019ba1e6
skylerzhang <skylerzhang@tencent.com>
2023-06-28 19:59:08 +0800

    rename & clean code

--- tests/zshm_loader_test/zshm_loader_test.cpp
+++ tests/zshm_loader_test/zshm_loader_test.cpp
@@ -32,12 +32,12 @@ s32 shm_loader_base_test()
 {
     u32 mem_size = sizeof(shm_header);
 
-    zshm_loader loader_svr;
-    zshm_loader loader_clt;
+    zshm_loader loader_svr(false, 1987090, mem_size);
+    zshm_loader loader_clt(false, 1987090, mem_size);
 
 
-    ASSERT_TEST(!loader_svr.check_exist(1987090, mem_size));
-    ASSERT_TEST(loader_svr.create_from_shm() == 0);
+    ASSERT_TEST(loader_svr.check() != 0);
+    ASSERT_TEST(loader_svr.create() == 0);
     new (loader_svr.shm_mnt_addr()) shm_header;
     shm_header* header = (shm_header*)loader_svr.shm_mnt_addr();
     for (u32 i = 0; i < 100; i++)
@@ -48,8 +48,8 @@ s32 shm_loader_base_test()
 
 
 
-    ASSERT_TEST(loader_clt.check_exist(1987090, mem_size));
-    ASSERT_TEST(loader_clt.load_from_shm() == 0);
+    ASSERT_TEST(loader_clt.check() == 0);
+    ASSERT_TEST(loader_clt.attach() == 0);
     
     header = (shm_header*)loader_clt.shm_mnt_addr();
 
@@ -63,9 +63,8 @@ s32 shm_loader_base_test()
     ASSERT_TEST(loader_clt.destroy() == 0);
 
 
-    loader_clt.reset();
-    ASSERT_TEST(!loader_clt.check_exist(1987090, mem_size));
-
+    loader_clt.init(false, 1987090, mem_size);
+    ASSERT_TEST(loader_clt.check() != 0);
 
     return 0;
 }
@@ -76,10 +75,10 @@ s32 shm_loader_unix_test()
 {
     u32 mem_size = sizeof(shm_header);
 
-    zshm_loader loader_svr;
+    zshm_loader loader_svr(false, 1987090, mem_size);
 
-    ASSERT_TEST(!loader_svr.check_exist(1987090, mem_size));
-    ASSERT_TEST(loader_svr.create_from_shm(0x0000700000000000) == 0);
+    ASSERT_TEST(loader_svr.check()!= 0);
+    ASSERT_TEST(loader_svr.create(0x0000700000000000) == 0);
     if (loader_svr.shm_mnt_addr() != (void*)0x0000700000000000ULL)
     {
         LogError() << "mnt addr:" << loader_svr.shm_mnt_addr();
@@ -93,7 +92,7 @@ s32 shm_loader_unix_test()
     }
     ASSERT_TEST(loader_svr.detach() == 0);
     ASSERT_TEST(loader_svr.destroy() == 0);
-    ASSERT_TEST(!loader_svr.check_exist(1987090, mem_size));
+    ASSERT_TEST(loader_svr.check() != 0);
     return 0;
 }
 
@@ -104,13 +103,12 @@ s32 shm_loader_stress_test()
 {
     u32 mem_size = sizeof(shm_header);
 
-    zshm_loader shm_loader;
-    zshm_loader heap_loader(true);
-    ASSERT_TEST(!shm_loader.check_exist(198709, mem_size));
-    ASSERT_TEST(shm_loader.create_from_shm() == 0);
-    ASSERT_TEST(!heap_loader.check_exist(198709, 2000*1024*1024));
-    //ASSERT_TEST(!heap_loader.check_exist(198709, mem_size));
-    ASSERT_TEST(heap_loader.create_from_shm() == 0);
+    zshm_loader shm_loader(false, 198709, mem_size);
+    zshm_loader heap_loader(true, 198709, 2000 * 1024 * 1024);
+    ASSERT_TEST(shm_loader.check() != 0);
+    ASSERT_TEST(shm_loader.create() == 0);
+    ASSERT_TEST(heap_loader.check() != 0);
+    ASSERT_TEST(heap_loader.create() == 0);
 
     shm_header& shm = *(shm_header*)shm_loader.shm_mnt_addr();
     shm_header& heap = *(shm_header*)heap_loader.shm_mnt_addr();
