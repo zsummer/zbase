a83dac5d380b6c3f41cbc373eba4cd6f4f2166a0
zsummer <yawei.zhang@foxmail.com>
2024-09-23 19:15:35 +0800

    more dynamic_cast test

--- tests/sys_platform_test/sys_platform_test.cpp
+++ tests/sys_platform_test/sys_platform_test.cpp
@@ -65,6 +65,63 @@ public:
 	float a = 0.0f;
 };
 
+class DeriveBB : public DeriveB
+{
+public:
+	DeriveBB()
+	{
+
+	}
+	virtual ~DeriveBB()
+	{
+
+	}
+	float a = 0.0f;
+};
+
+class DeriveAA : public DeriveA
+{
+public:
+	DeriveAA()
+	{
+
+	}
+	virtual ~DeriveAA()
+	{
+
+	}
+	float a = 0.0f;
+};
+
+
+class DeriveAAA : public DeriveAA
+{
+public:
+	DeriveAAA()
+	{
+
+	}
+	virtual ~DeriveAAA()
+	{
+
+	}
+	float a = 0.0f;
+};
+
+class DeriveAAAA : public DeriveAAA
+{
+public:
+	DeriveAAAA()
+	{
+
+	}
+	virtual ~DeriveAAAA()
+	{
+
+	}
+	float a = 0.0f;
+};
+
 Base* genObj(int a)
 {
 	if (a == 1)
@@ -76,6 +133,10 @@ Base* genObj(int a)
 		return new DeriveB();
 
 	}
+	if (a == 3)
+	{
+		return new DeriveAAAA();
+	}
 	return new Base();
 }
 
@@ -85,26 +146,77 @@ int main(int argc, char* argv[])
 {
     ztest_init();
 
-	Base* a = genObj(1);
+	Base* a = genObj(3);
 
 	volatile int c = 10000;
 
 	volatile int sum = 0;
 
+	if (true)
+	{
+		PROF_DEFINE_AUTO_MULTI_ANON_RECORD(cost, c * 2, "empty");
+		for (int i = 0; i < c; i++)
+		{
+			sum++;
+			sum--;
+		}
+	}
+
 
 	if (true)
 	{
-		PROF_DEFINE_AUTO_MULTI_ANON_RECORD(cost, c, "dynamic");
+		PROF_DEFINE_AUTO_MULTI_ANON_RECORD(cost, c*2, "dynamic_cast");
 		for (int i = 0; i < c; i++)
 		{
-			DeriveB* aa = dynamic_cast<DeriveB*>(a);
+			DeriveA* aa = dynamic_cast<DeriveA*>(a);
 			if (aa)
 			{
 				sum++;
 			}
+			DeriveB* bb = dynamic_cast<DeriveB*>(a);
+			if (bb)
+			{
+				sum--;
+			}
 		}
-
 	}
+
+	if (true)
+	{
+		PROF_DEFINE_AUTO_MULTI_ANON_RECORD(cost, c * 2, "static_cast");
+		for (int i = 0; i < c; i++)
+		{
+			DeriveA* aa = static_cast<DeriveA*>(a);
+			if (aa)
+			{
+				sum++;
+			}
+			DeriveB* bb = static_cast<DeriveB*>(a);
+			if (bb)
+			{
+				sum--;
+			}
+		}
+	}
+
+	if (true)
+	{
+		PROF_DEFINE_AUTO_MULTI_ANON_RECORD(cost, c * 2, "dynamic_cast AABB ");
+		for (int i = 0; i < c; i++)
+		{
+			DeriveAAAA* aa = dynamic_cast<DeriveAAAA*>(a);
+			if (aa)
+			{
+				sum++;
+			}
+			DeriveBB* bb = dynamic_cast<DeriveBB*>(a);
+			if (bb)
+			{
+				sum--;
+			}
+		}
+	}
+
 
 
 
