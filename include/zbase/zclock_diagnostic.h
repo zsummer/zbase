078ffc729df9b0979a7e61af359a3ae13f196194
zsummer <yawei.zhang@foxmail.com>
2024-09-23 08:44:30 +0000

    [skip ci]auto dist

---
+++ include/zbase/zclock_diagnostic.h
@@ -0,0 +1,33 @@
+
+/*
+* Copyright (C) 2019 YaweiZhang <yawei.zhang@foxmail.com>.
+* All rights reserved
+* This file is part of the zbase, used MIT License.
+*/
+
+
+
+
+#pragma once 
+#ifndef ZCLOCK_DIAGNOSTIC_H
+#define ZCLOCK_DIAGNOSTIC_H
+
+#include "zclock.h"
+
+
+class zclock_diagnostic
+{
+    zclock<> clock_;
+};
+
+
+
+
+
+
+
+
+
+
+
+#endif
