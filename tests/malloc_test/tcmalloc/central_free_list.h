99d958e16742d41af25664db5e4cd8eaf590e777
zsummer <yawei.zhang@foxmail.com>
2024-11-27 23:27:16 +0800

    add tcmalloc test

---
+++ tests/malloc_test/tcmalloc/central_free_list.h
@@ -0,0 +1,62 @@
+#pragma once
+
+
+#include "st_common.h"
+#include "span.h"
+
+class CentralFreeList
+{
+public:
+    CentralFreeList();
+    ~CentralFreeList();
+
+public:
+    int Init(SizeClass cl);
+    int InsertRange(void* start, void* end, int num);
+    int RemoveRange(void** start, void** end, int num);
+
+    // 返回从span中取得了多少对象
+    int FetchFromOneSpan(int num, void** start, void** end);
+    // 先尝试从span获取。span中不足，从page heap分配span,然后再获取
+    int FetchFromOneSpanSafe(int num, void** start, void** end);
+
+    int ReleaseListToSpans(void* start);
+    int ReleaseToSpans(void* object);
+
+    // 从page heap 中分配span,切分为cache对象
+    int Populate();
+
+    // 试图增大tc entry cache的数量
+    bool TryMakeCacheSpace();
+    // 收缩cache大小
+    int ShrinkCache(bool is_force);
+
+    // 统计所有碎片大小，数量的总和
+    unsigned long long OverheadBytes() const;
+    int TCTotalLength() const;
+
+    unsigned long long free_obj_num() const { return free_obj_num_; }
+
+    static bool EvictRandomSizeClass(SizeClass lock_cl, bool is_force);
+
+public:
+    struct TCEntry
+    {
+        void* head;
+        void* tail;
+    };
+
+    // 最大允许缓存多少条链表
+    static const int kMaxNumTransferEntries = 64;
+
+private:
+    SizeClass cl_;
+    Span empty_; // 已经全部分配出去的span
+    Span noempty_; // 部分分配出去的span, 还可以继续分配
+    unsigned long long num_spans_; // empty + noempty的span总和
+    TCEntry tc_slots_[kMaxNumTransferEntries];
+    int used_slots_; // 使用的tc_slots数目
+    int cache_size_; // 当前允许缓存slot大小
+    int max_cache_size_; // 最大允许缓存slot大小
+    unsigned long long free_obj_num_; // 空闲对象数量,包含span内的对象数量
+};
---
+++ tests/malloc_test/tcmalloc/page_heap.cpp
@@ -0,0 +1,115 @@
+#include <stddef.h>
+
+#include "st_common.h"
+#include "page_heap.h"
+#include "span.h"
+
+PageHeap::PageHeap():page_cache_(0)
+{
+
+}
+
+PageHeap::~PageHeap()
+{
+
+}
+
+int PageHeap::Init(PageMapType::NodeAllocator* node_allocator, PageMapType::LeafAllocator* leaf_allocator)
+{
+    if (NULL == node_allocator || NULL == leaf_allocator)
+    {
+        error_tlog("invalid args.");
+        return -1;
+    }
+
+    int ret = 0;
+    ret = page_map_.Init(node_allocator, leaf_allocator);
+    if (ret != 0)
+    {
+        error_tlog("page map init failed,ret<%d>.", ret);
+        return ret;
+    }
+
+    return ret;
+}
+
+Span* PageHeap::New(Length num)
+{
+    if (num <= 0)
+    {
+        error_tlog("invalid args.");
+        return NULL;
+    }
+
+    int ret = 0;
+    int order = log2_ceil(num) + kPageShift;
+    void* ptr = STAllocPages(order);
+    if (NULL == ptr)
+    {
+        error_tlog("STAllocPages failed.");
+        return NULL;
+    }
+
+    Span* span = NewSpan(reinterpret_cast<unsigned long long>(ptr) >> kPageShift,  num);
+    if (NULL == span)
+    {
+        error_tlog("NewSpan failed.");
+        ret = STFreePages(ptr,  1ull<<order);
+        if (ret != 0)
+        {
+            error_tlog("STFreePages failed, ret<%d>.", ret);
+        }
+        return NULL;
+    }
+    span->sizeclass = 0;
+    page_map_.Reserve((unsigned long long)ptr >> kPageShift, num);
+
+    return span;
+}
+
+int PageHeap::Delete(Span* span)
+{
+    if (NULL == span)
+    {
+        error_tlog("invalid args.");
+        return -1;
+    }
+
+    STFreePages((void*)(span->start << kPageShift),  span->page_num<<kPageShift);
+    DeleteSpan(span);
+
+    return 0;
+}
+
+int PageHeap::RegisterSizeClass(Span* span, SizeClass sc)
+{
+    if (NULL == span || sc == 0)
+    {
+        error_tlog("invalid args.");
+        return -1;
+    }
+
+    span->sizeclass = sc;
+    for (Length i = 0; i < span->page_num; ++i)
+    {
+        page_map_.set(span->start + i, span);
+    }
+    return 0;
+}
+
+int PageHeap::RecordSpan(Span* span)
+{
+    if (NULL == span)
+    {
+        error_tlog("invalid args.");
+        return -1;
+    }
+
+    page_map_.set(span->start, span);
+    if (span->page_num > 1)
+    {
+        page_map_.set(span->start + span->page_num - 1, span);
+    }
+    return 0;
+}
+
