a9c8e6fcc184b927ed247d3ee327fccd4fb4a3f8
zsummer <yawei.zhang@foxmail.com>
2024-12-26 10:56:25 +0800

    zclock_diagnostic

--- src/include/zclock_diagnostic.h
+++ src/include/zclock_diagnostic.h
@@ -15,16 +15,64 @@
 #include "zclock.h"
 
 
-class zclock_diagnostic
+template<zclock_impl::clock_type _C = zclock_impl::T_CLOCK_VOLATILE_RDTSC>
+class zclock_diagnostic_ns
 {
-    zclock<> clock_;
+public:
+    //watchdog  ns  
+    explicit zclock_diagnostic_ns(long long watchdog, std::function<void(long long)> dog)
+    {
+        watchdog_ = watchdog;
+        dog_ = dog;
+        clock_.start();
+    }
+    ~zclock_diagnostic_ns()
+    {
+        if (dog_ == nullptr)
+        {
+            return;
+        }
+        long long ns = clock_.save().cost_ns();
+        if (ns >= watchdog_)
+        {
+            dog_(ns);
+        }
+    }
+private:
+    zclock<_C> clock_;
+    long long watchdog_;
+    std::function<void(long long)> dog_;
 };
 
-
-
-
-
-
+template<zclock_impl::clock_type _C = zclock_impl::T_CLOCK_VOLATILE_RDTSC>
+class zclock_diagnostic_ms 
+{
+public:
+public:
+    //watchdog  ms  
+    explicit zclock_diagnostic_ms(long long watchdog, std::function<void(long long)> dog)
+    {
+        watchdog_ = watchdog * 1000 * 1000;
+        dog_ = dog;
+        clock_.start();
+    }
+    ~zclock_diagnostic_ms()
+    {
+        if (dog_ == nullptr)
+        {
+            return;
+        }
+        long long ns = clock_.save().cost_ns();
+        if (ns >= watchdog_)
+        {
+            dog_(ns/1000/1000);
+        }
+    }
+private:
+    zclock<_C> clock_;
+    long long watchdog_;
+    std::function<void(long long)> dog_;
+};
 
 
 
